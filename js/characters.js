f82a5bbcad6e30728d71293e908819aefa9c469f
Vasilii Chugunov <naukomania@gmail.com>
2017-02-25 15:53:10 +0300

    added exit door, sword

--- js/characters.js
+++ js/characters.js
@@ -7,6 +7,7 @@ function Stage (id) {
 }
 
 function Luke (settings) {
+    var me = this;
     this.imgSrc='images/luke.png';
     this.id='luke';
 
@@ -14,6 +15,24 @@ function Luke (settings) {
     this.posY = settings.posY ? settings.posY : 0;
 
     this.__proto__ = new Person(this);
+    this.registerKeys=function() {
+        $(document).on('keydown', function(e){
+            switch(event.keyCode) {
+                case 87:  // w
+                me.posY-=10;
+                break;
+                case 83: // s
+                me.posY+=10;
+                break;
+                case 65: // a
+                me.posX-=10;
+                break;
+                case 68: // d
+                me.posX+=10;
+                break;
+            }
+        });
+    }
 }
 
 function C3po (settings) {
@@ -48,4 +67,22 @@ function Person (parent) {
         this.parent.jqEl.css('top', this.parent.posY+'px');
         this.parent.jqEl.css('left', this.parent.posX+'px');
     }
+}
+
+function RightBar(stage) {
+    stage.jqEl.append('<div id="right-bar"></div>');
+    this.jqEl=$('#right-bar');
+    this.stage=stage;
+}
+
+function Door(stage){
+    stage.jqEl.append('<div id="door"></div');
+    this.jqEl=$('#door');
+    this.stage=stage;
+}
+
+function Sword(stage){
+    stage.jqEl.append('<div id="sword"></div');
+    this.jqEl=$('#sword');
+    this.stage=stage;
 }
